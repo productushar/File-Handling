28111a7236fa0bd2499959b9d64c023cb98a1729
Tushar Tyagi <tushar.tyagi.dxb@gmail.com>
2021-11-06 21:25:45 -0700

    Create Binary-Updateitem.py

---
+++ Binary-Updateitem.py
@@ -0,0 +1,5 @@
+#Updating data in a binary file
+
+#For this case, we are taking the scenario of an item, its quantity, and the price per unit; Particularly, we will be updating the total
+
+import pickle
